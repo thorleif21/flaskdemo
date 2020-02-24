84e38a0d5d958c4d4f175a294ff2cb7e721affd5
Þorleifur Einarsson <thorleif21@gmail.com>
2020-02-24 03:02:15 +0000

    Breytingar úr video 6

--- blog_demo/forms.py
+++ blog_demo/forms.py
@@ -1,6 +1,8 @@
+# encoding='utf-8'
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from blog_demo.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -11,6 +13,16 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Þessi notandi er til. Veljið annan')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Þetta póstfang er til. Veljið annað')
 
 
 class LoginForm(FlaskForm):
