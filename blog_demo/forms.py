cce14bc9e171f74febd22d3a8188426afa3f5033
Þorleifur Einarsson <thorleif21@gmail.com>
2020-02-25 02:22:28 +0000

    video 7

--- blog_demo/forms.py
+++ blog_demo/forms.py
@@ -1,5 +1,7 @@
 # encoding='utf-8'
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from blog_demo.models import User
@@ -30,3 +32,23 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(),
+                                                   Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update profile picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if current_user.username != username.data:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Þessi notandi er til. Veljið annan')
+
+    def validate_email(self, email):
+        if current_user.email != email.data:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Þetta póstfang er til. Veljið annað')
