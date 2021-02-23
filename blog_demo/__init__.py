f73467626928ddd9ca39fc1338eeaaa8426d9771
Þorleifur Einarsson <thorleif21@gmail.com>
2021-02-23 02:54:45 +0000

    timeout

--- blog_demo/__init__.py
+++ blog_demo/__init__.py
@@ -12,6 +12,9 @@ bcrypt = Bcrypt()
 csrf = CSRFProtect()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
+login_manager.refresh_view = 'users.login'
+login_manager.needs_refresh_message = (u"Session timedout, please re-login")
+login_manager.needs_refresh_message_category = "info"
 login_manager.login_message_category = 'info'
 mail = Mail()
 
--- run.py
+++ run.py
@@ -1,6 +1,13 @@
 from blog_demo import create_app
+from datetime import timedelta
+from flask import session
 
 app = create_app()
+@app.before_request
+def before_request():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=5)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
